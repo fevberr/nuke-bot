2239b80ba3cea560fb55a56b70f7efcb0e716fdc
Fevber <nah.id.win.286h@gmail.com>
2026-09-04 23:50:37 -0600

    Add files via upload

---
+++ core/d/_72.py
@@ -0,0 +1,14 @@
+﻿import time
+
+class _72:
+    def __init__(self):
+        self.cmds = {}
+    def _73(self, uid):
+        now = time.time()
+        if uid not in self.cmds:
+            self.cmds[uid] = []
+        self.cmds[uid] = [t for t in self.cmds[uid] if now - t < 60]
+        if len(self.cmds[uid]) >= 10:
+            return True
+        self.cmds[uid].append(now)
+        return False
