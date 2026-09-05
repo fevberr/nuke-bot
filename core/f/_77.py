7175baf8d1361e4566d2cd863bf0f1b9785f0d21
Fevber <nah.id.win.286h@gmail.com>
2026-09-05 00:35:15 -0600

    re

---
+++ core/f/_77.py
@@ -0,0 +1,8 @@
+
+import discord
+import time
+
+class _77:
+    async def _78(self, bot):
+        bot.start_time = time.time()
+        await bot.change_presence(activity=discord.Game(name="USE NUMBERS 1-70"), status=discord.Status.dnd)
